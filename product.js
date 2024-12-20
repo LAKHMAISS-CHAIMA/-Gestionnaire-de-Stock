98306afe796e180547f503bdb15465bed48fd195
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2024-12-20 16:58:15 +0100

    creation des fichiers et installation de l'environnement et creation de class product

---
+++ product.js
@@ -0,0 +1,13 @@
+class Product {
+    constructor(id, name, description, quantity, price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    };
+}
+module.exports = Product;
+
+
+
