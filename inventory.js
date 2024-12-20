2bd11d9108ec67f99873295c0abc937e4a459fa7
LAKHMAISS CHAIMA <chaimaalakhmaiss23@gmail.com>
2024-12-20 17:06:15 +0100

    ajouter la méthode updateproduct

--- inventory.js
+++ inventory.js
@@ -23,4 +23,28 @@ class Inventory {
               console.log(dataproduit);
         }
   }
+
+  // : Pour mettre à jour un produit existant.
+  updateProduct() {
+    const id = Number(prompt("Entrez l'id du produit à mettre à jour: "));
+
+    if (id <=0 || id > dataproduit.length) {
+          console.log("Produit introuvable.");
+          return;
+    }
+
+    const newname = prompt("Entrez le nouveau nom : ");
+    const newdescription = prompt("Entrez la nouvelle description : ");
+    const newquantity = Number(prompt("Entrez la nouvelle quantité : "));
+    const newprice = Number(prompt("Entrez le nouveau prix : "));
+
+    dataproduit[id - 1].name = newname;
+    dataproduit[id - 1].description = newdescription;
+    dataproduit[id - 1].quantity = newquantity;
+    dataproduit[id - 1].price = newprice;
+
+    fs.writeFileSync("data.json", JSON.stringify(dataproduit, null, 4))
+
+    console.log("Produit mis à jour avec succès !");
+}
 }
